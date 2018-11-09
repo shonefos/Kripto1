d10bc6c7e66a8050665096d36367d05e6565cf3b
Nenad Savic <nenads1979@gmail.com>
2018-11-09 20:40:57 +0100

    Initial Commit

---
+++ Kripto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kripto
+//
+//  Created by Nenad Savic on 11/9/18.
+//  Copyright © 2018 Nenad Savic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
